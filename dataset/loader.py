8e6e0c715eaca8e72c6fc33aee45e7699a99e197
Axel Larsson <65452706+axla-io@users.noreply.github.com>
2025-05-10 15:20:58 -0400

    Finished data loading class

--- dataset/loader.py
+++ dataset/loader.py
@@ -1,4 +1,5 @@
 import os
+import random
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
@@ -23,44 +24,60 @@ class Loader:
     def __init__(self, options) -> None:
         self.options = options
     
+    def _get_split_prefix(self, index, test_length):
+        return "test" if index < test_length else "train"
+    
+    def clean_dir(self, dir):
+        for dirpath, dirnames, filenames in os.walk(dir):
+            for file in filenames:
+                os.remove(os.path.join(dirpath, file))
+    
     def preprocess(self, in_dir, out_dir, file_names) -> None:
-        file_name = file_names[0]
-        # Get indices to use in testing dataset
+        # Shuffle the dataset and set limit for number of test images
+        random.shuffle(file_names)
         data_length = len(file_names)
         test_length = int(round(self.options.fraction * data_length))
-        train_length = data_length - test_length
-        
-        # Preprocess images
-        train_path = os.path.join(in_dir, "images", file_name)
-        self.preprocess_img(train_path)
 
-        # Preprocess ground truth
-        gt_path = os.path.join(in_dir, "masks", file_name)
-        self.preprocess_mask(gt_path)
+        for i,file_name in enumerate(file_names):
+            print(f"Processing file: {i+1} of {data_length}")
 
+            # Preprocess images
+            img_path_in = os.path.join(in_dir, "images", file_name)
+            img = self.preprocess_img(img_path_in)
 
-        # Output image to 
-       
+            # Preprocess ground truth mask
+            gt_path_in = os.path.join(in_dir, "masks", file_name)
+            mask = self.preprocess_mask(gt_path_in)
+
+            # Split dataset
+            prefix = self._get_split_prefix(i, test_length)
+
+            # Get output paths
+            img_path_out = os.path.join(out_dir, prefix,"images", file_name)
+            gt_path_out = os.path.join(out_dir, prefix,"masks", file_name)
+
+            # Output processed files
+            cv2.imwrite(img_path_out, img)
+            cv2.imwrite(gt_path_out, mask)
     
     def preprocess_img(self, file_path) -> None:
         # Preprocess data
-        train_img = cv2.imread(file_path)
+        train_img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
         small_img = cv2.resize(train_img, (self.options.w,self.options.w)) # Resize first to make subsequent operations faster
-        gray_img = cv2.cvtColor(small_img, cv2.COLOR_BGR2GRAY)
-        normalized_gray_img = cv2.normalize(gray_img, None, 0, 1, cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-        print(normalized_gray_img.shape)
-
+        normalized_gray_img = cv2.normalize(small_img, None, 0, 1, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        return normalized_gray_img
     
     def preprocess_mask(self, file_path) -> None:
-        gt_img = cv2.imread(file_path)
+        gt_img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
         small_img = cv2.resize(gt_img, (self.options.w,self.options.w))
         binary_mask = (small_img > 127).astype(np.uint8)
-        print(binary_mask.shape)
+        return binary_mask
 
 if __name__ == "__main__":
-    options = LoaderOptions(w = 128)
+    options = LoaderOptions(w = 256)
     loader = Loader(options)
     in_dir = "data/original"
-    out_dir = "data"
+    out_dir = "data/processed"
+    loader.clean_dir(out_dir)
     file_names = os.listdir(os.path.join(in_dir,"images"))
     loader.preprocess(in_dir, out_dir, file_names)
